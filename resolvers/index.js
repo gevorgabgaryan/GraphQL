86e19ef62e3b36b9400658b1aa62065a9bd5a5e5
Gevorg Abgaryan <gevorg.gak@gmail.com>
2024-04-05 13:36:11 +0400

    register

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,7 +1,37 @@
+import UserModel from "../models/UserModel";
+
 const resolvers = {
     Query: {
-      hello: () => 'Hello world',
+      users: async () => {
+         try{
+            return await UserModel.find();
+         } catch(e) {
+            throw e;
+         }
+      },
     },
-  };
+
+    Mutation : {
+      regUser: async (arg1, args) => {
+          try {
+              const user = await UserModel.findOne({email: args.email});
+              if (user) throw new Error('User already exists');
+              const newUser = await UserModel.create({
+                  firstName: args.firstName,
+                  lastName: args.lastName,
+                  email: args.email,
+                  role: 'user',
+                  status: "active",
+                  password: args.password
+              })
+
+              newUser.id = newUser._id;
+              return newUser;
+          } catch (error) {
+              throw error;
+          }
+      },
+    }
+  }
 
 export default resolvers;
--- schemas/index.js
+++ schemas/index.js
@@ -1,8 +1,22 @@
 import { buildSchema } from "graphql";
 
 const schemas = buildSchema(`
+    type User {
+        id: String!
+        firstName: String
+        lastName: String
+        role: String!
+        status: String!
+        email: String!
+        password: String!
+    }
+
     type Query {
-        hello: String
+        users: [User]
+    }
+
+    type Mutation {
+        regUser(email: String!, password: String!, firstName: String, lastName: String): User!
     }
 `)
 
